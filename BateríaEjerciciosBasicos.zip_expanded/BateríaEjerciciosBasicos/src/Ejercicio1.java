521105ec94c1ba20f26ccc120d2443b7040927c7
AdriValles <adrivalles4444@gmail.com>
2024-02-15 19:21:56 +0100

    h

--- BateríaEjerciciosBasicos.zip_expanded/BateríaEjerciciosBasicos/src/Ejercicio1.java
+++ BateríaEjerciciosBasicos.zip_expanded/BateríaEjerciciosBasicos/src/Ejercicio1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Ejercicio1 {
-//prueba
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce un numero");
